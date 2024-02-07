2494defe89850b78e8acf477d3679deff1f9b4a1
Merieme Bouisri <meriemebouisri@gmail.com>
2024-02-07 09:24:11 -0500

    ADD : Latex LSP support

--- lua/lsp/servers/latex.lua
+++ lua/lsp/servers/latex.lua
@@ -1,3 +1,40 @@
-return {
+local err_prefix = "[lsp/servers/latex.lua]"
 
+-- -- -- 
+
+local _lspcfg, lspcfg = pcall(require, 'lspconfig')
+if not _lspcfg then
+	print(err_prefix .. "Failed to load lspconfig")
+	return
+end
+
+local _lspcmp, lspcmp = pcall(require, 'cmp_nvim_lsp')
+if not _lspcmp then
+	print(err_prefix .. "Failed to load cmp_nvim_lsp")
+	return
+end
+-- -- -- 
+
+latex_engine = {
+	pdflatex = "pdflatex",
+	lualatex = "lualatex",
 }
+
+lspcfg["texlab"].setup({
+	capabilities = lspcmp.default_capabilities(),
+	settings = {
+		texlab = {
+			diagnosticsDelay = 50,
+			build = {
+				executable = latex_engine.pdflatex,
+				args = { '-synctex=1', '-halt-on-error', '-interaction=batchmode', '%f' },
+				onSave = true
+			},
+			chktex = {
+				onEdit = true
+			}
+		},
+
+	}
+})
+
